8c270f9af413871516d238e9ac30026217feff71
Gabriel Weymouth <gabriel.weymouth@gmail.com>
2023-09-28 14:28:33 +0200

    Got Bsplines running on GPUs
    Parameterizing the spline type by the degree and then using that as a static parameter in the recursive Cox de Boor allows the GPU to compile and run! Almost all tests passing.
    
    The derivatives at the end points seem to be broken, and this is causing the location test to fail.

--- example/example_01_complexBody.jl
+++ example/example_01_complexBody.jl
@@ -1,77 +1,79 @@
-function coxDeBoor(knots, u, k, d, count)
-    """
-        coxDeBoor(knots, u, k, d, count)
-
-    Compute the Cox-De Boor recursion for B-spline basis functions.
-
-    The `coxDeBoor` function computes the Cox-De Boor recursion for B-spline basis functions,
-    used in the evaluation of B-spline curves and surfaces.
-
-    Arguments:
-    - `knots`: An array of knot values.
-    - `u`: The parameter value at which to evaluate the B-spline basis function.
-    - `k`: The index of the current knot interval.
-    - `d`: The degree of the B-spline basis function.
-    - `count`: The number of control points.
-
-    Returns:
-    The value of the B-spline basis function at parameter `u` and knot interval `k`.
-    """
-    if (d == 0)
-        return Int(((knots[k+1] <= u) && (u < knots[k+2])) || ((u >= (1.0-1e-12)) && (k == (count-1))))
-    end
-    return (((u-knots[k+1])/max(√eps(u), knots[k+d+1]-knots[k+1]))*coxDeBoor(knots, u, k, (d-1), count)
-        + ((knots[k+d+2]-u)/max(√eps(u), knots[k+d+2]-knots[k+2]))*coxDeBoor(knots, u, (k+1), (d-1), count))
-end
-
 """
-    BSplineCurve(cps; degree=3, mem=Array)
+    BSplineCurve(cps; degree=3)
 
-Define a B-spline curve.
+Define a uniform B-spline curve.
 - `cps`: A 2D array representing the control points of the B-spline curve
 - `degree`: The degree of the B-spline curve
-- `mem`: Array memory type
 """
-struct BSplineCurve{A<:AbstractArray,V<:AbstractVector} <: Function
-    cps::A
+struct BSplineCurve{d,A<:AbstractArray,V<:AbstractVector} <: Function
+    pnts::A
     knots::V
-    count::Int
-    degree::Int
 end
-function BSplineCurve(cps;degree=3) 
-    count,T = size(cps, 2),promote_type(eltype(cps),Float32)
-    knots = [zeros(T,degree); collect(T,range(0, count-degree) / (count-degree)); ones(T,degree)]
-    BSplineCurve(cps,SA[knots...],count,degree)
-end
-function (l::BSplineCurve)(s,t) 
-    ```
-    Evaluate the spline function
-    - `s` position along the spline
-    - `t` time is currently unused but needed for ParametricBodies
-    ```
-    pt = zero(l.cps[:,1])
-    for k in range(0, l.count-1)
-        pt += coxDeBoor(l.knots, s, k, l.degree, l.count) * l.cps[:, k+1]
-    end
-    return pt
-end
-using Adapt
-Adapt.adapt_structure(to, x::BSplineCurve) = BSplineCurve(adapt(to,x.cps),adapt(to,x.knots),x.count,x.degree)
-
-# Define square using degree=1 BSpline.
 using StaticArrays
-cps = SA[5 5 0 -5 -5 -5  0  5 5
-         0 5 5  5  0 -5 -5 -5 0]
-square = BSplineCurve(cps,degree=1)
+function BSplineCurve(pnts;degree=3)
+    count,T = size(pnts, 2),promote_type(eltype(pnts),Float32)
+    knots = SA{T}[[zeros(degree); collect(range(0, count-degree) / (count-degree)); ones(degree)]...]
+    BSplineCurve{degree,typeof(pnts),typeof(knots)}(pnts,knots)
+end
+"""
+    (::BSplineCurve)(s,t)
+
+Evaluate the spline function
+- `s` position along the spline
+- `t` time is currently unused but needed for ParametricBodies
+"""
+function (l::BSplineCurve{d})(u::T,t) where {T,d}
+    u==1 && return l.pnts[:,end]
+    pt = SA{T}[0, 0]
+    for k in 1:size(l.pnts, 2)
+        pt += Bd(l.knots,u,k,Val(d))*l.pnts[:,k]
+    end
+    pt
+end
+Bd(knots, u, k, ::Val{0}) = 1
+function Bd(knots, u, k, ::Val{d}) where d
+    B = 0
+    knots[k]<u<knots[k+d] && (B+=(u-knots[k])/(knots[k+d]-knots[k])*Bd(knots,u,k,Val(d-1)))
+    knots[k+1]≤u<knots[k+d+1] && (B+=(knots[k+d+1]-u)/(knots[k+d+1]-knots[k+1])*Bd(knots,u,k+1,Val(d-1)))
+    return B
+end
+
+square = BSplineCurve(SA[5 5 0 -5 -5 -5  0  5 5
+                         0 5 5  5  0 -5 -5 -5 0],degree=1)
+@assert square(0.,0) ≈ [5,0]
+@assert square(.5,0) ≈ [-5,0]
+@assert square(1.,0) ≈ [5,0]
+
+# Does it work with KernelAbstractions?
+using CUDA; @assert CUDA.functional()
+using KernelAbstractions
+@kernel function _test!(a::AbstractArray,l::BSplineCurve)
+    # Map index to physical space
+    I = @index(Global)
+    s = (I-1)/(length(a)-1)
+    q = l(s,0)
+    a[I] = q'*q
+end
+test!(a,l)=_test!(get_backend(a),64)(a,l,ndrange=length(a))
+a = CUDA.zeros(64)
+test!(a,square)
+a|>Array # yes!
 
 # Check bspline is type stable. Note that using Float64 for cps will break this!
 @assert isa(square(0.,0),SVector)
 @assert all([eltype(square(zero(T),0))==T for T in (Float32,Float64)])
 
-# Create curve and heck winding direction
+# check winding direction
 using LinearAlgebra
 cross(a,b) = det([a;;b])
 @assert all([cross(square(s,0.),square(s+0.1,0.))>0 for s in range(0,.9,10)])
+
+# check derivatives
+using ForwardDiff
+dcurve(u) = ForwardDiff.derivative(u->square(u,0),u)
+dcurve(0f0)
+dcurve(0.5f0)
+dcurve(1)
 
 # Wrap the shape function inside the parametric body class and check measurements
 using ParametricBodies
@@ -82,8 +84,7 @@ body = ParametricBody(square, (0,1));
 # Check that the locator works for closed splines
 @assert [body.locate([5,s],0) for s ∈ (-2,-1,-0.1)]≈[0.95,0.975,0.9975]
 
-# Use mem=CUDA
-using CUDA; @assert CUDA.functional()
-body = ParametricBody(square, (0,1); T=Float32, mem=CUDA.CuArray) # doesn't work.
-# @assert all(measure(body,[1,2],0) .≈ [-3,[0,1],[0,0]])
-# @assert all(measure(body,[8,2],0) .≈ [3,[1,0],[0,0]])
+# Does it work with ParametricBodies on CUDA?
+body = ParametricBody(square, (0,1); T=Float32);
+CUDA.@allowscalar @assert all(measure(body,[1,2],0) .≈ [-3,[0,1],[0,0]])
+CUDA.@allowscalar @assert all(measure(body,[8,2],0) .≈ [3,[1,0],[0,0]]) # yes!
